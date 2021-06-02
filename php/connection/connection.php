929cf9fdd8b228db0cc469540eb6b87f13d1698c
mgakun <matheus.gomesdalmeida@gmail.com>
2021-06-02 14:42:48 -0300

    Subir arquivos

--- php/connection/connection.php
+++ php/connection/connection.php
@@ -1,12 +1,15 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$bd = "crud_php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password ,$bd);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
 ?>
