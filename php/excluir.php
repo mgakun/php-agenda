89cd854994571704f59ca42a0fc853af4497352b
mgakun <matheus.gomesdalmeida@gmail.com>
2021-06-10 21:20:40 -0300

    Atualização de segurança

--- php/excluir.php
+++ php/excluir.php
@@ -2,14 +2,18 @@
     include ("connection\connection.php");
     
     $id = $_GET['id'];
-    
+    $status = "I";
 
-    $sql = 
-    "UPDATE `cadastros` SET `status` = 'I' WHERE id = '$id'";
+    $stmt = $conn->prepare("UPDATE `cadastros` SET `status` = ? WHERE id = ?");
+    $stmt->bindParam(1, $status,PDO::PARAM_STR );
+    $stmt->bindParam(2, $id,PDO::PARAM_INT );
+    $stmt->execute();
 
-   if(mysqli_query($conn, $sql)) {
-    header('location:../index.php?excluido');
-   }
+    if($stmt->rowCount() > 0) {
+        header('location:../index.php?excluido');
+    }else{
+        header('location:../index.php?erro');
+    }
     
 
 ?>
--- php/update.php
+++ php/update.php
@@ -8,14 +8,20 @@
     $telefone = $_POST['telefone'];
     
 
-    $sql = 
-    "UPDATE `cadastros` SET `nome` = '$nome', `email` = '$email', `endereco` = '$endereco', `telefone` = '$telefone' WHERE id = '$id' ";
-
-    // echo  "UPDATE `cadastros` SET `nome` = '$nome', `email` = '$email', `endereco` = '$endereco', `telefone` = '$telefone', `data_nasc` = '$data_nasc' WHERE `id` = '$id'";exit;
-
-   if(mysqli_query($conn, $sql)) {
-    header('location:../index.php?atualizado');
-   }
+    $stmt = $conn->prepare("UPDATE `cadastros` SET `nome` = ?, `email` = ?, `endereco` = ?, `telefone` = ? WHERE id = ? ");
+    $stmt->bindParam(1, $nome,PDO::PARAM_STR );
+    $stmt->bindParam(2, $email,PDO::PARAM_STR );
+    $stmt->bindParam(3, $endereco,PDO::PARAM_STR );
+    $stmt->bindParam(4, $telefone,PDO::PARAM_STR );
+    $stmt->bindParam(5, $id,PDO::PARAM_INT );
+    $stmt->execute();
+    
+    if ($stmt->rowCount() > 0) {
+        header('location:../index.php?atualizado');
+    }else{
+            header('location:../index.php?erro');
+        }
+    
     
 
 ?>
